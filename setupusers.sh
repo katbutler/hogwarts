83f77902b3f2fb4a1d565b74ed7d725b9b63c47f
Kat Butler <kat.a.butler@gmail.com>
2024-03-27 21:55:23 -0400

    Update setupusers.sh

--- setupusers.sh
+++ setupusers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Usage: sudo setupusers.sh <names.txt>
 
-set -xe
+set -e
 
 F=$1
 
@@ -14,8 +14,15 @@ NAMES=$(cat $F)
 
 for name in ${NAMES[@]}
 do
-  echo $name
+  echo "adding $name ..."
+  set +e
+  if id "$name" >/dev/null 2>&1; then
+    echo "$name already exists. Skipping"
+    continue
+  fi
+  set -e
   useradd -d "/home/$name" -s /bin/bash -R "/home/$name" -U -m --skel /home/hpotter "$name"  
+  echo "added $name"
 done
 
 echo "Done creating all users"
